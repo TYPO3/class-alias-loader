d54ce982276ada33833eab4844b00c417af67661
Helmut Hummel <helmut.hummel@typo3.org>
2015-01-30 14:51:30 +0100

    Initial commit

---
+++ Classes/Composer/ClassAliasLoader.php
@@ -0,0 +1,125 @@
+<?php
+namespace Helhum\ClassAliasLoader\Composer;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Helmut Hummel <helmut.hummel@typo3.org>
+ *  All rights reserved
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Composer\Autoload\ClassLoader as ComposerClassLoader;
+
+/**
+ * Class ClassAliasLoader
+ */
+class ClassAliasLoader {
+
+	/**
+	 * @var ComposerClassLoader
+	 */
+	protected $composerClassLoader;
+
+	/**
+	 * @var array
+	 */
+	protected $aliasMap = array();
+
+	/**
+	 * @param ComposerClassLoader $composerClassLoader
+	 */
+	public function __construct(ComposerClassLoader $composerClassLoader) {
+		$composerClassLoader->unregister();
+		$this->composerClassLoader = $composerClassLoader;
+	}
+
+	/**
+	 * @return ComposerClassLoader
+	 */
+	public function getComposerClassLoader() {
+		return $this->composerClassLoader;
+	}
+
+	/**
+	 * @param array $aliasMap
+	 */
+	public function setAliasMap(array $aliasMap) {
+		$this->aliasMap = $aliasMap;
+	}
+
+	/**
+	 * @param array $aliasMap
+	 */
+	public function addAliasMap(array $aliasMap) {
+		foreach ($aliasMap['aliasToClassNameMapping'] as $alias => $class) {
+			$lowerCaseAlias = strtolower($alias);
+			$this->aliasMap['aliasToClassNameMapping'][$lowerCaseAlias] = $class;
+			$this->aliasMap['classNameToAliasMapping'][$class][$lowerCaseAlias] = $lowerCaseAlias;
+		}
+	}
+
+	/**
+	 * @param string $className
+	 * @return bool
+	 */
+	public function loadClassWithAlias($className) {
+		// Work around for PHP 5.3.0 - 5.3.2 https://bugs.php.net/50731
+		if ('\\' === $className[0]) {
+			$className = substr($className, 1);
+		}
+		$lowerCasedClassName = strtolower($className);
+		// Is an original class which has an alias
+		if (isset($this->aliasMap['classNameToAliasMapping'][$className])) {
+			return $this->loadOriginalClassAndSetAliases($className);
+		// Is an alias (we're graceful regarding casing for alias definitions)
+		} elseif (isset($this->aliasMap['aliasToClassNameMapping'][$lowerCasedClassName])) {
+			$originalClassName = $this->aliasMap['aliasToClassNameMapping'][$lowerCasedClassName];
+			return $this->loadOriginalClassAndSetAliases($originalClassName);
+		}
+		return $this->composerClassLoader->loadClass($className);
+	}
+
+	/**
+	 * @param string $originalClassName
+	 * @return bool
+	 */
+	protected function loadOriginalClassAndSetAliases($originalClassName) {
+		if ($this->composerClassLoader->loadClass($originalClassName)) {
+			foreach ($this->aliasMap['classNameToAliasMapping'][$originalClassName] as $aliasClassName) {
+				if (!class_exists($aliasClassName, false)) {
+					class_alias($originalClassName, $aliasClassName);
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Act as a proxy for method calls to composer class loader
+	 *
+	 * @param string $method
+	 * @param array $arguments
+	 * @return mixed
+	 */
+	public function __call($method, $arguments) {
+		if (!is_callable(array($this->composerClassLoader, $method))) {
+			throw new \InvalidArgumentException(sprintf('Method "%s" does not exist!', $method), 1422631610);
+		}
+		return call_user_func_array(array($this->composerClassLoader, $method), $arguments);
+	}
+
+}
