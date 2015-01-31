487a64f53bc2361b904a57b4255dd010ad561c4c
Helmut Hummel <helmut.hummel@typo3.org>
2015-01-31 12:29:48 +0100

    Fix license in source code and add LICENSE file

--- Classes/Composer/ClassAliasGenerator.php
+++ Classes/Composer/ClassAliasGenerator.php
@@ -1,25 +1,14 @@
 <?php
 namespace Helhum\ClassAliasLoader\Composer;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the class alias loader package.
  *
- *  (c) 2015 Helmut Hummel <helmut.hummel@typo3.org>
- *  All rights reserved
+ * (c) Helmut Hummel <info@helhum.io>
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the text file GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
- *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Composer\Util\Filesystem;
 
